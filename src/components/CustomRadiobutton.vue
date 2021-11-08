89e3c0928d45e1500de8bff0fbe93537164e13e0
Tatiana Maltseva <tatmal.educ@gmail.com>
2021-11-08 21:45:22 +0300

    added custom radiobutton

---
+++ src/components/CustomRadiobutton.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="custom-radio">
+    <input
+      type="radio"
+      v-model="radioButtonValue"
+      :name="name"
+      :value="value"
+    />
+    <label :for="name">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomRadiobutton",
+  model: {
+    prop: "checked",
+    event: "change",
+  },
+  props: {
+    checked: {
+      type: [String, Boolean],
+    },
+    value: {
+      type: [String, Boolean],
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    radioButtonValue: {
+      get() {
+        return this.checked;
+      },
+      set() {
+        this.setValue();
+      },
+    },
+  },
+  methods: {
+    setValue() {
+      this.$emit("change", this.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.custom-radio {
+  display: flex;
+}
+</style>
