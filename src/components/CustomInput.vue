af12ad722e568abffafe03137557912623f6d956
Tatiana Maltseva <tatmal.educ@gmail.com>
2021-11-08 21:38:54 +0300

    added custom input

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="custom-input">
+    <label :for="name">{{ label }}</label>
+    <input type="text" :name="name" :value="value" @input="setValue" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomInput",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+  },
+  inheritAttrs: false,
+  methods: {
+    setValue(event) {
+      this.$emit("input", event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.custom-input {
+  display: flex;
+  flex-direction: column;
+}
+</style>
